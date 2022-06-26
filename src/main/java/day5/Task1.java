a7e490e69eaff015281bd3076d0ef4fc0ecedbf2
Ivan Mazurkevich <Hendful@Yandex.ru>
2022-06-26 18:08:46 +0300

    added day 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,37 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setModel("KIA RIO");
+        car1.setColor("красный");
+        car1.setYearRelease(2017);
 
+        System.out.println("Марка автомобиля: " +car1.getModel()+ ", имеет " +car1.getColor()+ " цвет. Выпуск " +car1.getYearRelease()+ " года.");
+    }
+}
+class Car{
+    private String model;
+    private String color;
+    private int yearRelease;
+
+    public void setModel(String carModel){
+        model = carModel;
+    }
+    public String getModel() {
+        return model;
+    }
+
+    public void setColor(String carColor) {
+        color = carColor;
+    }
+    public String getColor() {
+        return color;
+    }
+
+    public void setYearRelease(int carYearRelease) {
+        yearRelease = carYearRelease;
+    }
+    public int getYearRelease() {
+        return yearRelease;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,31 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike1 = new Motorbike("Honda", "зелёный", 1987);
 
+        System.out.println("Модель мотоцикла: " +motorbike1.getModel());
+        System.out.println("Цвет мотоцикла: " +motorbike1.getColor());
+        System.out.println("Год выпуска: " +motorbike1.getYearRelease());
+    }
+}
+class Motorbike{
+    private String model;
+    private String color;
+    private int yearRelease;
+
+    public Motorbike(String model, String color, int yearRelease){
+        this.model = model;
+        this.color = color;
+        this.yearRelease = yearRelease;
+
+    }
+    public String getModel(){
+        return model;
+    }
+    public String getColor() {
+        return color;
+    }
+    public int getYearRelease() {
+        return yearRelease;
     }
 }
