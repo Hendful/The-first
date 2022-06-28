e096a583d9c6f217499ed137d3685fa1a2ead7f2
Ivan Mazurkevich <Hendful@Yandex.ru>
2022-06-28 22:15:38 +0300

    added day 7

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -2,6 +2,9 @@ package day7;
 
 public class Task1 {
     public static void main(String[] args) {
+        Airplane airbus380 = new Airplane("Airbus A380", 2004, 67.90, 280.5);
+        Airplane boeing747 = new Airplane("Boeing 747-8", 2011, 76.25, 300.1);
+        Airplane.compareAirplanes(airbus380, boeing747);
 
     }
 }
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,81 @@
 package day7;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+        Random random = new Random();
+        int randomPlayerStamina = random.nextInt(10) + 91;
 
+        Player player1 = new Player(randomPlayerStamina);
+        Player player2 = new Player(randomPlayerStamina);
+        Player player3 = new Player(randomPlayerStamina);
+        Player player4 = new Player(randomPlayerStamina);
+        Player player5 = new Player(randomPlayerStamina);
+        Player player6 = new Player(randomPlayerStamina);
+
+        Player.info();                                                   //
+        System.out.println(player1.getStamina());                        //
+        for (int i = 0; i < player1.getStamina(); i = 0) {               //
+            player1.run();                                               //     <<< проверка. По моему всё прекрасно!
+        }                                                                //
+        System.out.println(player1.getStamina());                        //
+        Player.info();                                                   //
+
+    }
+}
+
+class Player {
+    private int stamina;
+    private static final int MAX_STAMINA = 100;
+    private static final int MIN_STAMINA = 0;
+    private static int countPlayers;
+
+    public Player(int stamina) {
+        this.stamina = stamina;
+
+        if (countPlayers < 6)
+            countPlayers++;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public static int getCountPlayers() {
+        return countPlayers;
+    }
+
+    public void run() {
+        if (stamina == 0)
+            return;
+
+        stamina--;
+
+        if (stamina == 0)
+            countPlayers--;
+
+    }
+
+    public static void info() {
+        switch (countPlayers) {
+            case 5:
+                System.out.println("Команды неполные. На поле 1 свободное место");
+                break;
+            case 4:
+                System.out.println("Команды неполные. На поле 2 свободных места");
+                break;
+            case 3:
+                System.out.println("Команды неполные. На поле 3 свободных места");
+                break;
+            case 2:
+                System.out.println("Команды неполные. На поле 4 свободных места");
+                break;
+            case 1:
+                System.out.println("Команды неполные. На поле 5 свободных мест");
+                break;
+            default:
+                System.out.println("На поле нет свободных мест");
+        }
     }
 }
